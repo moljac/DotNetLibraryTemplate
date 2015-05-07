b17df662ec2917c5cb9479713c9254badebaf759
moljac <mcvjetko@holisticware.net>
2015-05-07 09:03:51 +0200

    initial commit   dos batch scripts wirh for loop   bash scripts for Mac OSX

--- build.cmd
+++ build.cmd
@@ -1,4 +1,57 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "DotNetLibraryTemplate.sln"
+@echo off
 
-REM For building symbols
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Debug "DotNetLibraryTemplate.sln"
+set VSVARS="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
+::set VSDEVCMD="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat"
+set MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+
+:: %comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat""
+
+call %VSVARS% 
+::call %VSDEVCMD% 
+
+set SOLUTIONS=^
+	"HolisticWare.System.Data.DataSetExtensions.sln" ^
+	
+.nuget\nuget.exe ^
+		restore HolisticWare.System.Data.DataSetExtensions.sln
+
+	.nuget\nuget.exe restore ..\%%s
+
+	
+pause		
+FOR %%s IN (%SOLUTIONS%) DO (
+	echo "=========================================================================="
+	echo Solution = %%s
+	
+	.nuget\nuget.exe restore ..\%%s
+		
+	.nuget\nuget.exe ^
+		update ^
+		..\%%s
+	
+	pause
+	::@IF %ERRORLEVEL% NEQ 0 PAUSE	
+	echo "--------------------------------------------------------------------"
+		
+	msbuild ^
+		/m:8 ^
+		/p:Configuration=Debug ^
+		%%s
+
+	msbuild ^
+		/m:8 ^
+		/p:Configuration=Release ^
+		%%s
+	
+	%MSBUILD% ^
+		/m:8 ^
+		/p:Configuration=Debug ^
+		%%s
+		
+	%MSBUILD% ^
+		/m:8 ^
+		/p:Configuration=Release ^
+		%%s
+)
+
+@IF %ERRORLEVEL% NEQ 0 PAUSE	
--- package.cmd
+++ package.cmd
@@ -1,3 +1,35 @@
-.nuget\nuget pack "src\SomeClassLibrary\SomeClassLibrary.csproj" -Symbols -OutputDirectory artifacts -Build -IncludeReferencedProjects -NonInteractive
+@echo off
 
-.nuget\nuget pack "src\SomeOtherClassLibrary\SomeOtherClassLibrary.csproj" -Symbols -OutputDirectory artifacts -Build -IncludeReferencedProjects -NonInteractive
+set NUGET=.nuget\nuget
+
+del /q artifacts\*.*
+
+:: this nuget wiil be unpacked and folder structure used to pack the last one
+::	*Unpack.nuspec is for packaging structure to be copied to duplo-dep-nugets 
+
+SET NUSPECS=^
+	"src\HolisticWare.System.Data.DataSetExtensions.nuspec" ^
+
+
+FOR %%n IN (%NUSPECS%) DO (
+	echo "=========================================================================="
+	echo NUSPEC = %%n
+	
+	%NUGET% ^
+		pack ^
+		%%n ^
+		-Symbols ^
+		-OutputDirectory artifacts ^
+		-Build ^
+		-IncludeReferencedProjects ^
+		-NonInteractive
+		
+	pause
+)	
+	
+echo ========================================================================
+dir artifacts\
+echo ========================================================================
+	
+@IF %ERRORLEVEL% NEQ 0 PAUSE	
+
